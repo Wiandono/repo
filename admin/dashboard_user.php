6578e1099fc8c50f9f327743d2b023162ef0aa88
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-05-21 18:34:48 +0700

    Preparing for Ads functionality

--- admin/dashboard_user.php
+++ admin/dashboard_user.php
@@ -9,7 +9,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Dashboard - Manage User</title>
+    <title>Manage User</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/dashboard.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
@@ -58,7 +58,7 @@
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a href="?a=ads">
                       <span class="glyphicon glyphicon-shopping-cart"></span>Manage Ads
                     </a>
                   </li>
