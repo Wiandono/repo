92b1aebc34fe2b08821f1d0d752e1b4c5a4e89e6
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-05-21 18:38:09 +0700

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+  include "c_admin.php";
+
+  $controller = new c_admin();
+
+  if (empty($_SESSION['username'])) {
+    $controller->viewLogin();
+  } else {
+    if (isset($_GET['l'])) {
+      $controller->logout();
+    } else if(isset($_GET['d'])) {
+      $id = $_GET['d'];
+      $controller->deleteUser($id);
+    } else if (isset($_GET['a'])) {
+      $controller->viewAds();
+    } else if(isset($_GET['u'])) {
+      $controller->viewUser();
+    } else {
+      $controller->invoke();
+    }
+  }
+ ?>
