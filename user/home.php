de0ac16f6e3f20c80d924dd340f1b792aacd4192
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-05-22 22:14:56 +0700

    Registration and login integration complete

--- user/home.php
+++ user/home.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/home.css" rel="stylesheet">
+    <link href="css/modal.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/home.js"></script>
@@ -17,7 +18,93 @@
       <div class="container">
         <div class="row row1">
           <ul class="largenav pull-right">
-            <li class="upper-links"><a class="links" href="#">Registrasi</a></li>
+            <li class="upper-links">
+              <a class="links" href="#" data-toggle="modal" data-target="#myModal">Registrasi</a>
+            </li>
+            <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    <h4 class="modal-title" id="myModalLabel" style="color:black">Registration - <a href="#">NgemilDulu</a></h4>
+                  </div>
+                  <div class="modal-body">
+                    <div class="row">
+                      <div class="col-md-8" style="border-right: 1px dotted #C2C2C2;padding-right: 30px;">
+                        <!-- Nav tabs -->
+                        <ul class="nav nav-tabs">
+                          <li class="active">
+                            <a href="#Registration" data-toggle="tab">Registration</a>
+                          </li>
+                        </ul>
+                        <!-- Tab panes -->
+                        <div class="tab-content">
+                          <div class="tab-pane active" id="Registration">
+                            <br>
+                            <form role="form" class="form-horizontal">
+                              <div class="form-group" style="color:black">
+                                <label for="email" class="col-sm-2 control-label">Username</label>
+                                <div class="col-sm-10">
+                                  <input type="text" class="form-control" placeholder="Username"/>
+                                </div>
+                              </div>
+                              <div class="form-group" style="color:black">
+                                <label for="email" class="col-sm-2 control-label">Email</label>
+                                <div class="col-sm-10">
+                                  <input type="email" class="form-control" id="email" placeholder="Email"/>
+                                </div>
+                              </div>
+                              <div class="form-group" style="color:black">
+                                <label for="password" class="col-sm-2 control-label">Password</label>
+                                <div class="col-sm-10">
+                                  <input type="password" class="form-control" id="password" placeholder="Password"/>
+                                </div>
+                              </div>
+                              <div class="form-group" style="color:black">
+                                <label for="password" class="col-sm-2 control-label">Confirm Password</label>
+                                <div class="col-sm-10">
+                                  <input type="password" class="form-control" id="password" placeholder="Confirm Password"/>
+                                </div>
+                              </div>
+                              <div class="row">
+                                <div class="col-sm-2">
+                                </div>
+                                <div class="col-sm-10">
+                                  <button type="button" class="btn btn-primary btn-sm">Save & Continue</button>
+                                  <button type="button" class="btn btn-default btn-sm">Cancel</button>
+                                </div>
+                              </div>
+                            </form>
+                          </div>
+                        </div>
+                        <div id="OR" class="hidden-xs">OR</div>
+                      </div>
+                      <div class="col-md-4">
+                        <div class="row text-center sign-with">
+                          <div class="col-md-12" style="color:black">
+                            <h3>Already have an account ?</h3>
+                          </div>
+                          <div class="col-md-12">
+                            <div class="form-group">
+                              <label class="sr-only" for="exampleInputEmail2">Email address</label>
+                              <input autocomplete="off" type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
+                            </div>
+                            <div class="form-group">
+                              <label class="sr-only" for="exampleInputPassword2">Password</label>
+                              <input autocomplete="off" type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
+                              <div class="help-block text-right"><a href="">Forget the password ?</a></div>
+                            </div>
+                            <div class="form-group">
+                              <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             <li class="dropdown upper-links">
               <a href="#" class="dropdown-toggle upper-links" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
               <ul id="login-dp" class="dropdown-menu">
