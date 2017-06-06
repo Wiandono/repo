528171bdb9252549ecc349c19383d485c80123e8
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-07 00:07:07 +0700

    Code cleaning

--- user/check_password.php
+++ user/check_password.php
@@ -1,8 +1,4 @@
 <?php
-  include "m_user.php";
-
-  $model = new m_user();
-
   if(!empty($_POST["cpassword"]) && !empty($_POST["password"])) {
     if($_POST["cpassword"] != $_POST["password"]) {
       echo "<span style='color:#D60202;'>Not Match</span>";
