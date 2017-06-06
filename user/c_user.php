26d154776665f030c43749758ebb8bacb44b82b1
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-07 00:06:23 +0700

    Updated for Form Validation functionality

--- user/c_user.php
+++ user/c_user.php
@@ -19,9 +19,21 @@
       }
     }
 
-    function getRegistrationForm() {
-      $_SESSION['username'] = $_POST['username'];
-      header("location: ?r=register");
+    function getRegistrationForm($username, $email, $password, $cpassword) {
+      $valid = $this->model->validateForm($username, $email, $password, $cpassword);
+
+      if ($valid == "invalidU") {
+        echo "<script>alert('Username yang anda masukkan sudah terdaftar!')</script>";
+      } elseif ($valid == "invalidE") {
+        echo "<script>alert('Email yang anda masukkan sudah terdaftar!')</script>";
+      } elseif ($valid == "invalidP") {
+        echo "<script>alert('Password belum memenuhi standar!')</script>";
+      } elseif ($valid == "invalidCP") {
+        echo "<script>alert('Password yang anda masukkan tidak sama!')</script>";
+      } else {
+        $_SESSION['username'] = $_POST['username'];
+        header("location: ?r=register");
+      }
     }
   }
  ?>
