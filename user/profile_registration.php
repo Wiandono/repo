736ba5e819ecfef540604159fd753c7340d4c438
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-07 04:59:24 +0700

    Form data transfer completed

--- user/profile_registration.php
+++ user/profile_registration.php
@@ -58,15 +58,6 @@
             <li class='dropdown upper-links'>
               <a href='#' class='dropdown-toggle upper-links' data-toggle='dropdown'><span class='glyphicon glyphicon-user'></span> My Account <span class='caret'></span></a>
               <ul class='dropdown-menu upper-links' role='menu'>
-                <li class='divider'></li>
-                <li><a href='#'>Profile</a></li>
-                <li><a href='#'>Messages</a></li>
-                <li><a href='#'>Manage Ads</a></li>
-                <li class='divider'></li>
-                <li><a href='#'>Account Settings</a></li>
-                <li class='divider'></li>
-                <li><a href='#'>Logout</a></li>
-                <li class='divider'></li>
               </ul>
             </li>
           </ul>
@@ -82,11 +73,7 @@
               <button class="flipkart-navbar-button col-xs-1"><span class="glyphicon glyphicon-search"></span></button>
             </div>
           </div>
-          <div class="cart largenav col-sm-2">
-            <a class="cart-button">
-              <span class="glyphicon glyphicon-shopping-cart"></span> Cart <span class="item-number ">0</span>
-            </a>
-          </div>
+          <div class="cart largenav col-sm-2"></div>
         </div>
       </div>
     </div>
@@ -104,28 +91,7 @@
     <div class="container">
       <div class="row">
         <div class="col-sm-3">
-          <a href="#" class="btn btn-danger btn-block btn-compose-email">Compose Messages</a>
-          <ul class="nav nav-pills nav-stacked nav-email shadow mb-20">
-            <li>
-              <a href="#">
-                <i class="glyphicon glyphicon-inbox"></i> Inbox
-                <span class="label label-danger pull-right">7</span>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i class="glyphicon glyphicon-send"></i> Send Mail
-              </a>
-            </li>
-          </ul><!-- /.nav -->
-          <h5 class="nav-email-subtitle">More</h5>
           <ul class="nav nav-pills nav-stacked nav-email mb-20 rounded shadow">
-            <li>
-              <a href="#">
-                <i class="glyphicon glyphicon-barcode"></i> Manage Ads
-                <span class="label label-danger pull-right">3</span>
-              </a>
-            </li>
             <li class="active">
               <a href="#">
                 <i class="glyphicon glyphicon-user"></i> Account Settings
@@ -148,10 +114,15 @@
                   <br>
                   <div class="col-xs-12 col-sm-8">
                     <ul class="list-group">
-                      <li class="list-group-item">John Doe</li>
-                      <li class="list-group-item">23, June 1997</li>
-                      <li class="list-group-item"><i class="glyphicon glyphicon-phone"></i> 08111678036 </li>
-                      <li class="list-group-item"><i class="glyphicon glyphicon-envelope"></i> john@example.com </li>
+                      <?php
+                        $row = $this->model->fetch($data);
+                        echo "
+                          <li class='list-group-item'>$row[0]</li>
+                          <li class='list-group-item'>-</li>
+                          <li class='list-group-item'><i class='glyphicon glyphicon-phone'></i> -</li>
+                          <li class='list-group-item'><i class='glyphicon glyphicon-envelope'></i> $row[1]</li>
+                        ";
+                       ?>
                     </ul>
                   </div>
                 </div>
@@ -188,21 +159,21 @@
                     <div class="form-group">
                       <label class="col-md-4 control-label" for="textinput">E-mail</label>
                       <div class="col-md-4">
-                        <input id="textinput" name="textinput" placeholder="example@domain.com" class="form-control input-md" required="" type="text">
+                        <input id="textinput" name="textinput" placeholder="example@domain.com" class="form-control input-md" required="" type="text" value=<?php echo $row[1]; ?>>
                       </div>
                     </div>
                     <!-- Text input-->
                     <div class="form-group">
                       <label class="col-md-4 control-label" for="textinput">Password</label>
                       <div class="col-md-4">
-                        <input id="textinput" name="textinput" placeholder="Password" class="form-control input-md" required="" type="password">
+                        <input id="textinput" name="textinput" placeholder="Password" class="form-control input-md" required="" type="password" value=<?php echo $row[2]; ?>>
                       </div>
                     </div>
                     <!-- Text input-->
                     <div class="form-group">
                       <label class="col-md-4 control-label" for="textinput">Confirm Password</label>
                       <div class="col-md-4">
-                        <input id="textinput" name="textinput" placeholder="Confirm password" class="form-control input-md" required="" type="password">
+                        <input id="textinput" name="textinput" placeholder="Confirm password" class="form-control input-md" required="" type="password" value=<?php echo $row[2]; ?>>
                       </div>
                     </div>
                     <!-- File Button -->
