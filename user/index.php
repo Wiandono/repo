cf4157a0647e127bb65abe759293fae8e01c1ee3
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-07 23:45:15 +0700

    Added viewProfile function

--- user/index.php
+++ user/index.php
@@ -8,6 +8,8 @@
   } else {
     if (isset($_GET['r'])) {
       $controller->viewRegistration($_SESSION['username']);
+    } elseif (isset($_GET['e'])) {
+      $controller->viewProfile($_SESSION['username'], $_GET['e']);
     } else {
       $controller->viewHome("m");
     }
