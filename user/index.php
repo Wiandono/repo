c2d4352421485158ea9c225a5edd6d77b5cdde5f
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-08 22:30:28 +0700

    Added more view functionality

--- user/index.php
+++ user/index.php
@@ -10,6 +10,14 @@
       $controller->viewRegistration($_SESSION['username']);
     } elseif (isset($_GET['e'])) {
       $controller->viewProfile($_SESSION['username'], $_GET['e']);
+    } elseif (isset($_GET['l'])) {
+      $controller->logout();
+    } elseif (isset($_GET['m'])) {
+      $controller->viewMessage($_SESSION['username']);
+    } elseif (isset($_GET['s'])) {
+      $controller->viewSent($_SESSION['username']);
+    } elseif (isset($_GET['a'])) {
+      $controller->viewAds($_SESSION['username']);
     } else {
       $controller->viewHome("m");
     }
