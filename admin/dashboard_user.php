76875b2f6afc4aacd45f74a2fa0fc53d9164442f
Wiandono Saputro <wiandonosaputro@gmail.com>
2017-06-09 08:53:13 +0700

    Update dashboard_user.php

--- admin/dashboard_user.php
+++ admin/dashboard_user.php
@@ -35,7 +35,7 @@
     		<!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
           <ul class="nav navbar-nav navbar-right">
-            <li><a href="../index.php" target="_blank">Visit Site</a></li>
+            <li><a href="../user/" target="_blank">Visit Site</a></li>
             <li><a href="?l=logout" target="_self">Logout</a></li>
           </ul>
         </div><!-- /.navbar-collapse -->
